570c95e807901a364fbca537bcb65f72688129f7
SarahLM <s0550883@htw-berlin.de>
2020-11-02 22:15:37 +0100

    Storybook hinzugefügt

---
+++ webapp/src/components/ListItem/ListItem.stories.js
@@ -0,0 +1,24 @@
+import ListItem from "./ListItem.vue"
+
+export default {
+    title: 'Dirty/ListItem',
+    component: ListItem,
+    argTypes: {
+    },
+  };
+  
+  const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { ListItem},
+    template: '<list-item @update-item="updateItem" @delete-item="deleteItem" v-bind="$props" />',
+    
+});
+  
+  export const Base = Template.bind({});
+  Base.args = {
+      item:{
+        id: 0,
+        title: 'VueJS',
+        votes: 3
+      }
+  };
