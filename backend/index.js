2ac17410af10b1507f9c1780fca1dc4b403f61dd
Jenny <jennynz@web.de>
2020-11-11 17:57:54 +0100

    apollo test server setup in index added

--- backend/index.js
+++ backend/index.js
@@ -46,3 +46,47 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+
+const { createTestClient } = require('apollo-server-testing');
+
+const { query, mutate } = createTestClient(server);
+
+query({
+  query: GET_USER,
+  variables: { id: 1 }
+});
+
+mutate({
+  mutation: UPDATE_USER,
+  variables: { id: 1, email: 'nancy@foo.co' }
+});
+
+const { createTestClient } = require('apollo-server-testing');
+
+it('fetches single launch', async () => {
+  const userAPI = new UserAPI({ store });
+  const launchAPI = new LaunchAPI();
+
+  // create a test server to test against, using our production typeDefs,
+  // resolvers, and dataSources.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({ userAPI, launchAPI }),
+    context: () => ({ user: { id: 1, email: 'a@a.a' } }),
+  });
+
+  // mock the dataSource's underlying fetch methods
+  launchAPI.get = jest.fn(() => [mockLaunchResponse]);
+  userAPI.store = mockStore;
+  userAPI.store.trips.findAll.mockReturnValueOnce([
+    { dataValues: { launchId: 1 } },
+  ]);
+
+  // use the test server to create a query function
+  const { query } = createTestClient(server);
+
+  // run query against the server and snapshot the output
+  const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
+  expect(res).toMatchSnapshot();
+});
