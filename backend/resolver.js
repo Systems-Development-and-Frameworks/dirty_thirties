f1b77c23a01150c0eef67b5712328229b894d498
Jenny <jennynz@web.de>
2020-11-16 15:19:41 +0100

    first code changes, typedefs not ready yet

---
+++ backend/resolver.js
@@ -0,0 +1,52 @@
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    posts: () => posts,
+    users: () => users,
+  },
+
+  Post: {
+
+
+  },
+
+  User: {
+
+
+  },
+
+  Mutation: {
+    write (parent, args) {
+      //write the function
+      //return type Post
+    }
+
+    delete (parent, args) {
+      //write the function
+      //return type Post
+
+    }    
+
+    update (parent, args) {
+      //write the function
+      //return type Post
+
+    }
+    
+    downvote (parent, args) {
+      //write the function
+      //return type Post
+
+    }
+  }
+
+};
+
+//1. List all posts
+
+//2. List all users
+
+//3. Create a post
+
+//4. Upvote a post
