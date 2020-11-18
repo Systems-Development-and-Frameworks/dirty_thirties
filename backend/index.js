da4ae49145c8f1fdb041f76b4fb704fbc9973457
Jenny <jennynz@web.de>
2020-11-18 19:03:35 +0100

    extra test-file, InMemoryDataSource extended

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,38 @@ import resolver from './resolver.js'
 //Apollo Server
 const { ApolloServer, gql } = require('apollo-server');
 
-//not needed 
-const books = [
+//Konstanten User - evtl nicht nötig weil ja nicht konstant 
+const users = [
   {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+    name: 'Klaus Meier', //name of the user
+    postIds: [1, 2], //get List of Posts
   },
   {
-    title: 'City of Glass',
-    author: 'Paul Auster',
+    name: 'Maria Müller', //name of the user
+    postIds: [3], //get List of Posts
   },
 ];
+
+const posts = [
+  {
+    id: 1,
+    title: 'Post 1',
+    votes: 3, //Achtung, darf nicht statisch sein
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    votes: 7, //Achtung, darf nicht statisch sein
+  },
+  {
+    id: 3,
+    title: 'Post 3',
+    votes: 5, //Achtung, darf nicht statisch sein
+  },
+];
+
+//jede Person nur 1x upvoten - siehe Hints (Mengen) -> sets
+
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -29,59 +50,25 @@ server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
 
-const { createTestClient } = require('apollo-server-testing');
-
-const { query, mutate } = createTestClient(server);
-
-query({
-  query: GET_USER,
-  variables: { id: 1 }
-});
-
-mutate({
-  mutation: UPDATE_USER,
-  variables: { id: 1, email: 'nancy@foo.co' }
-});
-
-it('fetches single launch', async () => {
-  const userAPI = new UserAPI({ store });
-  const launchAPI = new LaunchAPI();
-
-  // create a test server to test against, using our production typeDefs,
-  // resolvers, and dataSources.
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => ({ userAPI, launchAPI }),
-    context: () => ({ user: { id: 1, email: 'a@a.a' } }),
-  });
-
-  // mock the dataSource's underlying fetch methods
-  launchAPI.get = jest.fn(() => [mockLaunchResponse]);
-  userAPI.store = mockStore;
-  userAPI.store.trips.findAll.mockReturnValueOnce([
-    { dataValues: { launchId: 1 } },
-  ]);
-
-  // use the test server to create a query function
-  const { query } = createTestClient(server);
-
-  // run query against the server and snapshot the output
-  const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
-  expect(res).toMatchSnapshot();
-});
-
 import { DataSource } from 'apollo-datasource'
 
 export default class InMemoryDataSource extends DataSource {
   constructor () {
     super()
+    //befüllen ggfs. this.
+    this.post = []
   }
 
-  initialize ({ context }) {}
+  initialize ({ context }) {
+    console.log('InMemoryDataSource: ', context)
+  }
 
-  allPosts () {}
-  createPost (data) {}
+  allPosts (data) {}
+  createPost (data) {
+    const newPost = new Post(data)
+    this.posts.push(newPost)
+    return newPost
+  }
   upvotePost(id, user) {}
 }
 
