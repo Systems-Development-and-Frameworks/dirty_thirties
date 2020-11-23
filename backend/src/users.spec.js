918438079301e69813d609b0b862f2389674edc6
Jenny <jennynz@web.de>
2020-11-23 18:50:41 +0100

    changes in users.spec.js

--- backend/src/users.spec.js
+++ backend/src/users.spec.js
@@ -1,46 +1,127 @@
+import { gql } from 'apollo-server'
+import { createTestClient } from 'apollo-server-testing'
+import Server from './server.js'
+import { InMemoryDataSource, User, Post } from './ds.js'
 
-//Test Case - in andere Datei
-const { createTestClient } = require('apollo-server-testing');
-
-const { query, mutate } = createTestClient(server);
-
-query({
-  query: GET_USER,
-  variables: { id: 1 }
-});
-
-mutate({
-  mutation: UPDATE_USER,
-  variables: { id: 1, email: 'nancy@foo.co' }
-});
-
-it('fetches single launch', async () => {
-  const userAPI = new UserAPI({ store });
-  const launchAPI = new LaunchAPI();
-
-  // create a test server to test against, using our production typeDefs,
-  // resolvers, and dataSources.
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => ({ userAPI, launchAPI }),
-    context: () => ({ user: { id: 1, email: 'a@a.a' } }),
-  });
-
-  // mock the dataSource's underlying fetch methods
-  launchAPI.get = jest.fn(() => [mockLaunchResponse]);
-  userAPI.store = mockStore;
-  userAPI.store.trips.findAll.mockReturnValueOnce([
-    { dataValues: { launchId: 1 } },
-  ]);
-
-  // use the test server to create a query function
-  const { query } = createTestClient(server);
-
-  // run query against the server and snapshot the output
-  const res = await query({ query: GET_LAUNCH, variables: { id: 1 } });
-  expect(res).toMatchSnapshot();
-});
+let ds
+beforeEach(() => {
+    ds = new InMemoryDataSource(),   //Komma norwendig?
+    ds.users.push(new User('Jenny V.'), new User('Sarah M.'))
 
 
+const server = new Server({ dataSources: () => ({ ds }) })
 
+const { query, mutate } = createTestClient(server)
+
+describe('queries', () => {
+    describe('USERS', () => {
+        const USERS = gql`
+            query {
+                users {
+                    name
+                }
+            }
+        `
+
+        it('given users in the database', async () => {
+            await expect(query({ query: USERS }))
+              .resolves
+              .toMatchObject({
+                errors: undefined,
+                data: { users: [{ name: 'Jenny V.' }, { name: 'Sarah M.' }] }
+            })
+        })
+    })
+
+    describe('USERS', () => {
+        const USERS = gql`
+            query {
+                users {
+                    name
+                    posts {
+                        title
+                    }
+                }
+            }
+        `
+
+        it('returns all users with no posts', async () => {
+            await expect(query({ query: USERS }))
+              .resolves
+              .toMatchObject({
+                errors: undefined,
+                data: {
+                    users: [
+                        { name: 'Jenny V.', posts: [] }, //muss hier ein Komma ?
+                        { name: 'Sarah M.', posts: [] }
+                    ]
+                }
+            })
+        })
+
+        describe('WRITE_POST', () => {
+            const action = () =>
+                mutate({
+                    mutation: WRITE_POST,
+                    variables: { post: { title: "Some post", author: { name: 'Jenny V.' } } }
+                })
+            const WRITE_POST = gql`
+                mutation($post: PostInput!) {
+                    write(post: $post) {
+                        author {
+                            name
+                            posts {
+                                title
+                            }
+                        }
+                    }
+                }
+            `
+            it('adds post to user', async () => {
+                await expect(action())
+                  .resolves
+                  .toMatchObject({
+                    errors: undefined,
+                    data: {
+                        write: { author: { name: 'Jenny V.', posts: [{ title: 'Some post' }] } }
+                    }
+                })
+            })
+        })
+
+        describe('DELETE_POST', () => {
+            let postId,
+            beforeEach(() => {
+                db.posts = [new Post({ title: 'Some post', authorName: 'Jenny V.' })],
+                postId = db.posts[0].id
+            });
+            const deletePost = () =>
+                mutate({
+                    mutation: DELETE_POST,
+                    variables: { id: postId }
+                })
+            const DELETE_POST = gql`
+                mutation($id: ID!) {
+                    delete(id: $id) {
+                        author {
+                            name
+                            posts {
+                                title
+                            }
+                        }
+                    }
+                }
+            `
+            it('removes post from a user', async () => {
+                await expect(deletePost())
+                  .resolves
+                  .toMatchObject({
+                    errors: undefined,
+                    data: {
+                        delete: { author: { name: 'Jenny V.', posts: [] } }
+                    }
+                })
+            })
+        })
+    })
+})
