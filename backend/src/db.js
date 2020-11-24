bb6ea9c2834f43b7c82a99b740363abe5ecee2fc
SarahLM <s0550883@htw-berlin.de>
2020-11-24 17:46:31 +0100

    fixed db, deleted promises

---
+++ backend/src/db.js
@@ -0,0 +1,77 @@
+import { DataSource } from 'apollo-datasource';
+import crypto from 'crypto';
+
+export class Post {
+  constructor (data) {
+    this.voters = new Set();
+    this.id = crypto.randomBytes(16).toString('hex');
+    Object.assign(this, data);
+  }
+}
+
+export class User {
+    constructor(name) {
+      this.id = crypto.randomBytes(16).toString('hex');
+      this.name = name;
+    }
+}
+
+export class InMemoryDataSource extends DataSource {
+  constructor () {
+    super();
+    console.log('constructor of the InMemoryDataSource');
+    this.posts = [];
+    this.users = [];
+  }
+
+  initialize(...args) {}
+
+  allUsers() {
+    return this.users;
+  }
+
+  getUser(id) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  allPosts() {
+    return this.posts;
+  }
+
+  getPost(id) {
+    return this.posts.find((post) => post.id === id);
+  }
+
+  createPost (data) {
+    const newPost = new Post(data);
+    this.posts.push(newPost);
+    return newPost;
+  }
+
+  deletePost(postId) {
+    // fitler post
+    const deletedPost = this.posts.find((post) => post.id == postId);
+
+    // delete Post from post array
+    this.posts = this.posts.filter((post) => post.id != postId);
+
+    return deletedPost;
+  }
+
+  //upvote a post
+  upvotePost(postid, userid) {
+    const post = this.getPost(postid);
+    // return if user has allready upvoted post
+    // if (post.voters.has(userid)) {
+    //   return null;
+    // }
+    post.voters.add(userid);
+    return post;
+  }
+
+  downvotePost(postid, userid) {
+    const post = this.getPost(postid);
+    post.voters.delete(userid);
+    return post;
+  }
+}
